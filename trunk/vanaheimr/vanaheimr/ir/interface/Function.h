42c869a73d499cd8a70c0a05a0cd57d0fa80b00a
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2012-02-03 20:51:23 +0000

    ADD: Added some preliminary code for functions and variables.

---
+++ trunk/vanaheimr/vanaheimr/ir/interface/Function.h
@@ -0,0 +1,26 @@
+/*! \file   Function.h
+	\date   Friday February 3, 2012
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	\brief  The header file for the Function class.
+*/
+
+#pragma once
+
+namespace vanaheimr
+{
+
+namespace ir
+{
+
+/*! \brief Describes a vanaheimr function */
+class Function : public Variable
+{
+public:
+
+};
+
+}
+
+}
+
+
