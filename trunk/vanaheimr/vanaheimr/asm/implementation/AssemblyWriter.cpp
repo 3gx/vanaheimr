1f8a3d70582325618cb6c6c2e5a222d4d8885176
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2012-03-06 06:44:09 +0000

    ENH: Minor edits to assembly writer.

--- trunk/vanaheimr/vanaheimr/asm/implementation/AssemblyWriter.cpp
+++ trunk/vanaheimr/vanaheimr/asm/implementation/AssemblyWriter.cpp
@@ -13,9 +13,37 @@ namespace vanaheimr
 namespace asm
 {
 
-AssemblyWriter::AssemblyWriter();
+AssemblyWriter::AssemblyWriter()
+{
 
-void AssemblyWriter::write(std::ostream& stream, const ir::Module& m);
+}
+
+void AssemblyWriter::write(std::ostream& stream, const ir::Module& module)
+{
+	report("Writing assembly for module '" << module.name << "'");
+
+	for(auto function : module)
+	{
+		writeFunction(stream, function);
+	}
+	
+	for(auto global = module.global_begin();
+		global != module.global_end(); ++global)
+	{
+		writeGlobal(stream, *global);
+	}
+}
+
+void AssemblyWriter::writeFunction(std::ostream& stream, const ir::Function& f)
+{
+	report(" For function '" << f.name << "'");
+}
+
+void AssemblyWriter::writeGlobal(std::ostream& stream, const ir::Global& g)
+{
+	report(" For global '" << g.name << "'");
+
+}
 
 }
 
