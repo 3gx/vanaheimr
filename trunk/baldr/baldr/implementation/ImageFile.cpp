e4d03c30ddbaa0b9842c5d4a2b68c5d750fcfb75
gregory.diamos@gatech.edu <gregory.diamos@gatech.edu@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2012-04-09 00:46:02 +0000

    ENH: Filled out the implementation of the image class.

--- trunk/baldr/baldr/implementation/ImageFile.cpp
+++ trunk/baldr/baldr/implementation/ImageFile.cpp
@@ -4,25 +4,88 @@
 	\brief  The source file for the ImageFile class.
 */
 
-
 // Baldr Includes
 #include <baldr/include/ImageFile.h>
 
+// LibPNG Includes
+#include <png.h>
+
+// Standard Library Includes
+#include <cstdio>
 
 namespace baldr
 {
 
-ImageFile::ImageFile(unsigned int width, unsigned int height);
+ImageFile::ImageFile(unsigned int width, unsigned int height)
+{
+	resize(width, height);
+}
 
-void ImageFile::clear();
-void ImageFile::resize(unsigned int width, unsigned int height);
+void ImageFile::clear()
+{
+	_width  = 0;
+	_heaght = 0;
+
+	_pixels.clear();
+}
+
+void ImageFile::resize(unsigned int width, unsigned int height)
+{
+	_width  = width;
+	_height = height;
+
+	_pixels.resize(width * height);
+}
+
 void ImageFile::setPixel(unsigned int x, unsigned int y,
 	unsigned int red, unsigned int green,
-	unsigned int blue, unsigned int alpha);
+	unsigned int blue, unsigned int alpha)
+{
+	_pixels[_getIndex(x, y)] = {reg, green, blue, alpha};
+}
 
-void ImageFile::write(const std::string& filename);
-unsigned int ImageFile::_getIndex(unsigned int x, unsigned int y);
+void ImageFile::write(const std::string& filename)
+{
+	FILE* file = std::open(filename.c_str(), "wb");
 
+	if(file == 0)
+	{
+		throw std::runtime_error("Could not open image file "
+			+ filename + " for writing.");
+	}
+	
+	png_structp writeStructure = png_create_write_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
+
+	if(writeStructure == 0)
+	{
+		throw std::runtime_error("Creating PNG write structure failed.");
+	}
+	
+	png_info* pngInfo = png_create_info_struct(writeStructure);
+
+	if(pngInfo == 0)
+	{
+		throw std::runtime_error("Creatig PNG info structure failed.");
+	}
+
+	png_init_io(writeStructure, file);
+
+	png_set_IHDR(writeStructure, pngInfo, _width, _height, 8, PNG_COLOR_TYPE_RGBA,
+		PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+	png_write_info(writeStructure, pngInfo);
+
+	png_write_image(writeStructure, _pixels.data());
+
+	png_write_end(writeStructure, 0);
+
+	std::fclose(file);
+}
+
+unsigned int ImageFile::_getIndex(unsigned int x, unsigned int y)
+{
+	return y * _height + w;
+}
 
 }
 
