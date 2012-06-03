55d6f92e15c2241f8aa00178e3b99b4bfe56d20c
mailsudnya@gmail.com <mailsudnya@gmail.com@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2012-06-03 08:41:41 +0000

    ENH: rendered sphere with ascii chars correctly

--- trunk/baldr/baldr/implementation/Renderer.cpp
+++ trunk/baldr/baldr/implementation/Renderer.cpp
@@ -15,9 +15,12 @@ namespace baldr
 {
     void Renderer::renderScene()
     {
-        XYZ sampleOffsetX = (m_viewport.m_dy.getRayEquation()).scalarDivide(m_width);
+        XYZ sampleOffsetX = (m_viewport.m_dx.getRayEquation()).scalarDivide(m_width);
         XYZ sampleOffsetY = (m_viewport.m_dy.getRayEquation()).scalarDivide(m_height);
 
+        std::cout << "Start renderer\n";
+        std::cout << "Camera at: " << m_camera.getX() << " , " << m_camera.getY() << " , " 
+            << m_camera.getZ() << "\n"; 
         for (unsigned h = 0; h < m_height; ++h)
         {
             for (unsigned w = 0; w < m_width; ++w)
@@ -25,7 +28,9 @@ namespace baldr
                 XYZ currentPixOffsetY = sampleOffsetY.scalarProduct(h);
                 XYZ currentPixOffsetX = sampleOffsetX.scalarProduct(w);
                 XYZ currentPos        = (currentPixOffsetX.add(currentPixOffsetY)).add(m_viewport.m_corner);
-                Ray testRay(currentPos, m_viewport.m_corner);
+                Ray testRay(currentPos, m_camera);
+//                std::cout << "Ray starts at : " << testRay.getRayStart().getX() << testRay.getRayStart().getY() << testRay.getRayStart().getZ() << "\n";
+//                std::cout << "Ray equation is :" << testRay.getRayEquation().getX() << "," << testRay.getRayEquation().getY() << "," << testRay.getRayEquation().getZ() << "\n"; 
 
                 for (ObjectsInScene::iterator obj = m_objects.begin(); obj != m_objects.end(); ++obj)
                 {
