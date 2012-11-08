840ccecac43ca967a19c332bdbb71050a6bd4134
gregory.diamos <gregory.diamos@gatech.edu>
2012-11-08 07:46:03 +0000

    ENH: Started adding knob support.

---
+++ archaeopteryx/archaeopteryx/util/test/TestFileAccessKernel.cu
@@ -0,0 +1,13 @@
+
+
+#include <archaeopteryx/util/interface/File.h>
+
+__global__ void main(const char* filename, void* result,
+	const void* data, unsigned int size)
+{
+	archaeopteryx::util::File file(filename);
+
+	file.write(data, size);
+	file.read(result, size);
+}
+
