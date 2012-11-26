8ad4bd257ee8705c8a984d2b85f20e9f1b1dbebd
solusstultus <SolusStultus@gmail.com>
2012-11-26 19:06:16 +0000

    ENH: Started on convert from SSA pass.

--- vanaheimr/vanaheimr/transforms/interface/ConvertFromSSAPass.h
+++ vanaheimr/vanaheimr/transforms/interface/ConvertFromSSAPass.h
@@ -1,1 +1,42 @@
-// TODO: this is a placeholder, not implemented yet
+/*! \file   ConvertFromSSAPass.h
+	\date   Tuesday November 20, 2012
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	\brief  The header file for the ConvertFromSSAPass class.
+*/
+
+#pragma once
+
+// Vanaheimr Includes
+#include <vanaheimr/transforms/interface/Pass.h>
+
+namespace vanaheimr
+{
+
+namespace transforms
+{
+
+class ConvertFromSSAPass : public FunctionPass
+{
+public:
+	ConvertFromSSAPass();
+
+public:
+	virtual void runOnFunction(Function& f);
+
+private:
+	void _removePhis(Function& f);
+	void _removePsis(Function& f);
+
+private:
+	void _removePhis(ir::BasicBlock& block);
+
+private:
+	void removePsi(ir::Psi& psi);
+
+};
+
+}
+
+}
+
+
