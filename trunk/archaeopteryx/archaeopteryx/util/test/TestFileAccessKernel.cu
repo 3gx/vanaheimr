fa37503717f633142e77513c63a5aa9267a06a2e
solusstultus@gmail.com <solusstultus@gmail.com@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2012-12-06 02:35:01 +0000

    ENH: Some tweaks to the nvcc build script and minor bug fixes.  Now uses the OcelotLinker for PTX.

--- trunk/archaeopteryx/archaeopteryx/util/test/TestFileAccessKernel.cu
+++ trunk/archaeopteryx/archaeopteryx/util/test/TestFileAccessKernel.cu
@@ -2,7 +2,7 @@
 
 #include <archaeopteryx/util/interface/File.h>
 
-__global__ void deviceMain(const char* filename, void* result,
+extern "C" __global__ void deviceMain(const char* filename, void* result,
 	const void* data, unsigned int size)
 {
 	archaeopteryx::util::File file(filename);
