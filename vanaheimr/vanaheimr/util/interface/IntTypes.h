8118e5dfca013731ee614699d0e0b67b2a849116
solusstultus <SolusStultus@gmail.com>
2012-12-07 18:14:21 +0000

    ENH: Added updated int types.

--- vanaheimr/vanaheimr/util/interface/IntTypes.h
+++ vanaheimr/vanaheimr/util/interface/IntTypes.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#ifdef __NVCC__
-	typedef           unsigned char   uint8_t;
-	typedef           unsigned short uint16_t;
-	typedef           unsigned int   uint32_t;
-	typedef long long unsigned int   uint64_t;
-#else
-	#include <cstdint>
-#endif
+#include <stdint.h>
+
 
