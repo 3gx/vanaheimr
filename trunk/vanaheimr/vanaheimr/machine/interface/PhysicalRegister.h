51aa9c4468a261c202db188d2d4e815e841a179c
solusstultus@gmail.com <solusstultus@gmail.com@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2013-01-22 22:09:24 +0000

    ENH: More machine model work.

--- trunk/vanaheimr/vanaheimr/machine/interface/PhysicalRegister.h
+++ trunk/vanaheimr/vanaheimr/machine/interface/PhysicalRegister.h
@@ -6,6 +6,10 @@
 
 #pragma once
 
+// Standard Library Includes
+#include <string>
+#include <vector>
+
 // Forward Declarations
 namespace vanaheimr { namespace machine { class RegisterFile; } }
 
@@ -24,16 +28,18 @@ public:
 public:
 	/*! \brief Construct a register associated with a register file */
 	PhysicalRegister(const RegisterFile* file,
-		Id id = 0, const std::string& name = "");
+		Id id = 0, Id uid = 0, const std::string& name = "");
 
 public:
 	const RegisterFile* registerFile() const;
 	Id                  id()           const;
+	Id                  uniqueId()     const;
 	std::string         name()         const;
 
 private:
 	const RegisterFile* _registerFile;
 	Id                  _id;
+	Id                  _uniqueId;
 	std::string         _name;
 };
 
