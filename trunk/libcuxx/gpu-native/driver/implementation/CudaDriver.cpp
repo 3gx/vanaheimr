d7ae16f6f1985a65182875f009c124e1ca959ef7
solusstultus@gmail.com <solusstultus@gmail.com@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2013-11-21 04:16:00 +0000

    ENH: More bug fixes, now ran into ptxas issues.

--- trunk/libcuxx/gpu-native/driver/implementation/CudaDriver.cpp
+++ trunk/libcuxx/gpu-native/driver/implementation/CudaDriver.cpp
@@ -7,6 +7,12 @@
 // GPU Native Includes
 #include <gpu-native/driver/interface/CudaDriver.h>
 
+#include <gpu-native/util/interface/Casts.h>
+#include <gpu-native/util/interface/debug.h>
+
+// System Specific Includes
+#include <dlfcn.h>
+
 namespace gpunative
 {
 
@@ -72,11 +78,11 @@ void CudaDriver::cuDeviceTotalMem(size_t* b, CUdevice d)
 	_checkResult((*_interface.cuDeviceTotalMem)(b, d));
 }
 
-void CudaDriver::cuDeviceGetProperties(CUdevprop* prop, CUdevice dev)
+void CudaDriver::cuDeviceGetProperties(CUdevprop* p, CUdevice d)
 {
 	_check();
 	
-	_checkResult((*_interface.cuDeviceTotalMem)(b, d));
+	_checkResult((*_interface.cuDeviceGetProperties)(p, d));
 }
 
 void CudaDriver::cuDeviceGetAttribute(int* p, CUdevice_attribute a, CUdevice d)
@@ -160,7 +166,7 @@ void CudaDriver::cuMemFree(CUdeviceptr p)
 {
 	_check();
 	
-	_checkResult((*_interface.cuMemAlloc)(p));
+	_checkResult((*_interface.cuMemFree)(p));
 }
 
 void CudaDriver::cuMemGetAddressRange(CUdeviceptr* p, size_t* d, CUdeviceptr dp)
@@ -184,46 +190,192 @@ void CudaDriver::cuMemFreeHost(void* p)
 	_checkResult((*_interface.cuMemFreeHost)(p));
 }
 
-void CudaDriver::cuMemHostAlloc(void** pp, 
-		unsigned long long bytesize, unsigned int Flags);
-void CudaDriver::cuMemHostRegister(void* pp, 
-		unsigned long long bytesize, unsigned int Flags);
-void CudaDriver::cuMemHostUnregister(void* pp);
+void CudaDriver::cuMemHostAlloc(void** p, unsigned long long b, unsigned int f)
+{
+	_check();
+	
+	_checkResult((*_interface.cuMemHostAlloc)(p, b, f));
+}
 
-void CudaDriver::cuMemHostGetDevicePointer(CUdeviceptr* pdptr, 
-		void* p, unsigned int Flags);
-void CudaDriver::cuMemHostGetFlags(unsigned int* pFlags, void* p);
-void CudaDriver::cuMemcpyHtoD (CUdeviceptr dstDevice, 
-		const void* srcHost, unsigned int ByteCount);
-void CudaDriver::cuMemcpyDtoH (void* dstHost, CUdeviceptr srcDevice, 
-		unsigned int ByteCount);
-void CudaDriver::cuFuncSetBlockShape (CUfunction hfunc, int x, 
-		int y, int z);
-void CudaDriver::cuFuncSetSharedSize (CUfunction hfunc, 
-		unsigned int bytes);
+void CudaDriver::cuMemHostRegister(void* p, unsigned long long b, unsigned int f)
+{
+	_check();
 
-void CudaDriver::cuParamSetSize(CUfunction hfunc, 
-		unsigned int numbytes);
-void CudaDriver::cuParamSetv(CUfunction hfunc, int offset, 
-		void*  ptr, unsigned int numbytes);
-void CudaDriver::cuLaunchGrid (CUfunction f, int grid_width, 
-		int grid_height);
-void CudaDriver::cuEventCreate(CUevent* phEvent, 
-		unsigned int Flags);
-void CudaDriver::cuEventRecord(CUevent hEvent, CUstream hStream);
-void CudaDriver::cuEventQuery(CUevent hEvent);
-void CudaDriver::cuEventSynchronize(CUevent hEvent);
-void CudaDriver::cuEventDestroy(CUevent hEvent);
-void CudaDriver::cuEventElapsedTime(float* pMilliseconds, 
-		CUevent hStart, CUevent hEnd);
-void CudaDriver::cuStreamCreate(CUstream* phStream, 
-		unsigned int Flags);
-void CudaDriver::cuStreamQuery(CUstream hStream);
-void CudaDriver::cuStreamSynchronize(CUstream hStream);
-void CudaDriver::cuStreamDestroy(CUstream hStream);
-std::string CudaDriver::toString(CUresult result);
+	_checkResult((*_interface.cuMemHostRegister)(p, b, f));
+}
 
-bool CudaDriver::doesFunctionExist(CUmodule hmod, const char* name);
+void CudaDriver::cuMemHostUnregister(void* p)
+{
+	_check();
+
+	_checkResult((*_interface.cuMemHostUnregister)(p));
+}
+
+void CudaDriver::cuMemHostGetDevicePointer(CUdeviceptr* d,
+	void* p, unsigned int f)
+{
+	_check();
+
+	_checkResult((*_interface.cuMemHostGetDevicePointer)(d, p, f));
+}
+
+void CudaDriver::cuMemHostGetFlags(unsigned int* f, void* p)
+{
+	_check();
+
+	_checkResult((*_interface.cuMemHostGetFlags)(f, p));
+}
+
+
+void CudaDriver::cuMemcpyHtoD(CUdeviceptr d, 
+	const void* s, unsigned int b)
+{
+	_check();
+
+	_checkResult((*_interface.cuMemcpyHtoD)(d, s, b));
+}
+
+void CudaDriver::cuMemcpyDtoH(void* d, CUdeviceptr s,
+	unsigned int b)
+{
+	_check();
+
+	_checkResult((*_interface.cuMemcpyDtoH)(d, s, b));
+}
+
+void CudaDriver::cuFuncSetBlockShape(CUfunction h, int x, 
+	int y, int z)
+{
+	_check();
+	
+	_checkResult((*_interface.cuFuncSetBlockShape)(h, x, y, z));
+}
+
+void CudaDriver::cuFuncSetSharedSize(CUfunction h, unsigned int b)
+{
+	_check();
+	
+	_checkResult((*_interface.cuFuncSetSharedSize)(h, b));
+}
+
+void CudaDriver::cuParamSetSize(CUfunction h, unsigned int n)
+{
+	_check();
+
+	_checkResult((*_interface.cuParamSetSize)(h, n));
+}
+
+void CudaDriver::cuParamSetv(CUfunction f, int o, void* p, unsigned int b)
+{
+	_check();
+	
+	_checkResult((*_interface.cuParamSetv)(f, o, p, b));
+}
+
+void CudaDriver::cuLaunchGrid (CUfunction f, int w, int h)
+{
+	_check();
+	
+	_checkResult((*_interface.cuLaunchGrid)(f, w, h));
+}
+
+void CudaDriver::cuEventCreate(CUevent* e, unsigned int f)
+{
+	_check();
+	
+	_checkResult((*_interface.cuEventCreate)(e, f));
+}
+
+void CudaDriver::cuEventRecord(CUevent e, CUstream s)
+{
+	_check();
+	
+	_checkResult((*_interface.cuEventRecord)(e, s));
+}
+
+void CudaDriver::cuEventQuery(CUevent e)
+{
+	_check();
+	
+	_checkResult((*_interface.cuEventQuery)(e));
+}
+
+void CudaDriver::cuEventSynchronize(CUevent e)
+{
+	_check();
+	
+	_checkResult((*_interface.cuEventSynchronize)(e));
+}
+
+void CudaDriver::cuEventDestroy(CUevent e)
+{
+	_check();
+	
+	_checkResult((*_interface.cuEventDestroy)(e));
+}
+
+void CudaDriver::cuEventElapsedTime(float* t, 
+		CUevent s, CUevent e)
+{
+	_check();
+	
+	_checkResult((*_interface.cuEventElapsedTime)(t, s, e));
+}
+
+std::string CudaDriver::toString(CUresult result)
+{               
+	switch(result)
+	{
+		case CUDA_SUCCESS: return "CUDA DRIVER - no errors";
+		case CUDA_ERROR_INVALID_VALUE: return "CUDA DRIVER - invalid value";
+		case CUDA_ERROR_OUT_OF_MEMORY: return "CUDA DRIVER - out of memory";
+		case CUDA_ERROR_NOT_INITIALIZED: return "CUDA DRIVER - driver not initialized";
+		case CUDA_ERROR_DEINITIALIZED: return "CUDA DRIVER - deinitialized";
+		case CUDA_ERROR_NO_DEVICE: return "CUDA DRIVER - no device";
+		case CUDA_ERROR_INVALID_DEVICE: return "CUDA DRIVER - invalid device";
+		case CUDA_ERROR_INVALID_IMAGE: return "CUDA DRIVER - invalid kernel image";
+		case CUDA_ERROR_INVALID_CONTEXT: return "CUDA DRIVER - invalid context";
+		case CUDA_ERROR_CONTEXT_ALREADY_CURRENT: return "CUDA DRIVER - context already current";
+		case CUDA_ERROR_MAP_FAILED: return "CUDA DRIVER - map failed";
+		case CUDA_ERROR_UNMAP_FAILED: return "CUDA DRIVER - unmap failed";
+		case CUDA_ERROR_ARRAY_IS_MAPPED: return "CUDA DRIVER - array is mapped";
+		case CUDA_ERROR_ALREADY_MAPPED: return "CUDA DRIVER - already mapped";
+		case CUDA_ERROR_NO_BINARY_FOR_GPU: return "CUDA DRIVER - no gpu binary";
+		case CUDA_ERROR_ALREADY_ACQUIRED: return "CUDA DRIVER - already aquired";
+		case CUDA_ERROR_NOT_MAPPED: return "CUDA DRIVER - not mapped";
+		case CUDA_ERROR_INVALID_SOURCE: return "CUDA DRIVER - invalid source";
+		case CUDA_ERROR_FILE_NOT_FOUND: return "CUDA DRIVER - file not found";
+		case CUDA_ERROR_INVALID_HANDLE: return "CUDA DRIVER - invalid handle";
+		case CUDA_ERROR_NOT_FOUND: return "CUDA DRIVER - not found";
+		case CUDA_ERROR_NOT_READY: return "CUDA DRIVER - not ready";
+		case CUDA_ERROR_LAUNCH_FAILED: return "CUDA DRIVER - launch failed";
+		case CUDA_ERROR_LAUNCH_OUT_OF_RESOURCES: return "CUDA DRIVER - out of resources";
+		case CUDA_ERROR_LAUNCH_TIMEOUT: return "CUDA DRIVER - launch timeout";
+		case CUDA_ERROR_LAUNCH_INCOMPATIBLE_TEXTURING: return "CUDA DRIVER - incompatible texturing";
+		case CUDA_ERROR_NOT_MAPPED_AS_POINTER: return "CUDA DRIVER - not mapped as pointer";
+		case CUDA_ERROR_UNKNOWN: return "CUDA DRIVER - unknown error";
+		default: break;
+	}
+	return "invalid_error";
+}
+
+bool CudaDriver::doesFunctionExist(CUmodule m, const char* n)
+{
+	_check();
+	
+	CUfunction f;
+
+	CUresult result = (*_interface.cuModuleGetFunction)(&f, m, n);
+
+	if(result == CUDA_ERROR_NOT_FOUND)
+	{
+		return false;
+	}
+	
+	_checkResult(result);
+
+	return true;
+}
 
 void CudaDriver::_check()
 {
@@ -241,7 +393,7 @@ static std::string errorCodeToString(CUresult r)
 {
 	std::stringstream stream;
 	
-	stream << r;
+	stream << "(" << r << ") : " << CudaDriver::toString(r);
 	
 	// TODO: add more info
 	
@@ -250,12 +402,14 @@ static std::string errorCodeToString(CUresult r)
 
 void CudaDriver::_checkResult(CUresult r)
 {
-	if(r != 0)
+	if(r != CUDA_SUCCESS)
 	{
 		throw std::runtime_error("libcuda API call returned error code: " +
 			errorCodeToString(r));
 	}
 }
+
+CudaDriver::Interface CudaDriver::_interface;
 
 CudaDriver::Interface::Interface()
 : _library(nullptr)
@@ -309,6 +463,8 @@ void CudaDriver::Interface::load()
 	DynLink(cuDeviceGetAttribute);
 	DynLink(cuCtxCreate);
 	DynLink(cuCtxDestroy);
+	DynLink(cuCtxGetApiVersion);
+	DynLink(cuCtxSynchronize);
 
 	DynLink(cuModuleLoadDataEx);
 	DynLink(cuModuleUnload);
@@ -317,19 +473,25 @@ void CudaDriver::Interface::load()
 	DynLink(cuFuncSetBlockShape);
 	DynLink(cuFuncSetSharedSize);
 
+	DynLink(cuMemGetInfo);
 	DynLink(cuMemAlloc);
 	DynLink(cuMemFree);
+	DynLink(cuMemGetAddressRange);
 	DynLink(cuMemAllocHost);
 	DynLink(cuMemFreeHost);
+
 	DynLink(cuMemHostAlloc);
 	DynLink(cuMemHostRegister);
 	DynLink(cuMemHostUnregister);
-
 	DynLink(cuMemHostGetDevicePointer);
+	DynLink(cuMemHostGetFlags);
+	
 	DynLink(cuMemcpyHtoD);
 	DynLink(cuMemcpyDtoH);
 
 	DynLink(cuParamSetv);
+	DynLink(cuParamSetSize);
+
 	DynLink(cuLaunchGrid);
 	DynLink(cuEventCreate);
 	DynLink(cuEventRecord);
---
+++ trunk/libcuxx/osx.sh
@@ -0,0 +1,17 @@
+# Get the current directory
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export LIBCUXX_INSTALL_PATH=$SCRIPTPATH/build_local
+
+export PATH=$LIBCUXX_INSTALL_PATH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=/usr/local/cuda/lib:$LIBCUXX_INSTALL_PATH/lib
+
+#if [ -n "$LLVM_INSTALL_PATH" ]
+#then
+#	export PATH=$LLVM_INSTALL_PATH/bin:$PATH
+#fi
+
+export CXX=clang++
+export CC=clang
+export CPP=clang
+
