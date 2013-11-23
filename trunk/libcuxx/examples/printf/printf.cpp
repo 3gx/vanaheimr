5d13866582cc9f07ab3869128970512031d051ea
solusstultus@gmail.com <solusstultus@gmail.com@4e4a93ee-0f40-11df-90bc-d5bccb450167>
2013-11-23 02:26:27 +0000

    ENH: Better debugging messages to the loader.

--- trunk/libcuxx/examples/printf/printf.cpp
+++ trunk/libcuxx/examples/printf/printf.cpp
@@ -5,7 +5,12 @@
 int main(int argc, char** argv)
 {
 	std::printf("Hello GPU\n");
-	
+
+	for(int i = 0; i < argc; ++i)
+	{
+		std::printf("Argument[%d] = '%s'\n", i, argv[i]);
+	}
+
 	return 0;
 }
 
