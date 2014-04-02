fbedb0f5bcd13ce16f0da42a3c51358cf88ea518
solusstultus <SolusStultus@gmail.com>
2014-04-02 20:49:54 +0000

    ENH: code refactoring.

---
+++ libcuxx/gpu-native/compiler/CompilerDriver/CLANG.py
@@ -0,0 +1,99 @@
+################################################################################
+#
+# \file   CLANG.py
+# \author Gregory Diamos <solusstultus@gmail.com>
+# \date   Tuesday June 25, 2013
+# \brief  The CLANG, controls the C++ compiler driver.
+#
+################################################################################
+
+# Compiler Modules
+from FileTypes import *
+
+# Standard Modules
+from   which    import which
+from   time     import time
+import os
+import subprocess
+
+# The CLANG Compiler class
+class CLANG:
+	def __init__(self, driver):
+		self.driver = driver
+
+	def compile(self, files):
+		
+		outputs = []
+		
+		for filename in files:
+			if self.canCompile(filename):
+				outputs.append(self.lower(filename))
+			else:
+				outputs.append(filename)
+		
+		return outputs, self.isFinished()
+
+	def isFinished(self):
+		return isPTX(self.driver.outputFile)
+
+	def getIntermediateFiles(self, filename):
+		if self.canCompile(filename):
+			if not self.isFinished():
+				return [self.getOutputFilename(filename)]
+		return []
+	
+	def canCompileFile(self, filename):
+		if isCPP(filename):
+			return True
+				
+		return False
+
+	def canCompile(self, filenames):
+		for filename in filenames:
+			if not canCompileFile(filename):
+				return False
+
+		return True
+
+	def getOutputFilename(self):
+		if self.isFinished():
+			return self.driver.outputFile
+		else:
+			return getBase(self.driver.outputFile) + '.ptx'
+
+	def lower(self, filenames):
+		assert self.canCompile(filenames)
+		
+		backend_path = which(getCLANGName())
+
+		outputFilename = self.getOutputFilename()
+
+		safeRemove(outputFilename)
+
+		command = backend_path + " -S -target nvptx64 " + ' '.join(filenames) + " -o " + \
+			outputFilename + " " + self.driver.getCompilerArguments()
+ 
+		if self.driver.verbose:
+			print 'Running ' + getCLANGName() + ' with: "' + command + '"'
+
+		start = time()
+
+		process = subprocess.Popen(command, shell=True,
+			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		(stdOutData, stdErrData) = process.communicate()
+
+		if self.driver.verbose:
+			print ' time: ' + str(time() - start) + ' seconds' 
+
+		if not os.path.isfile(outputFilename):
+			raise SystemError(getCLANGName() + ' failed to generate an output file: \n' \
+				+ stdOutData + stdErrData)
+
+		return outputFilename
+
+	def getCLANGName(self):
+		return 'clang++'
+
+
+
--- libcuxx/osx.sh
+++ libcuxx/osx.sh
@@ -8,7 +8,6 @@ then
 	export VANAHEIMR_INSTALL_PATH=$SCRIPTPATH/../vanaheimr/build_local 
 fi
 
-
 export PATH=$LIBCUXX_INSTALL_PATH/bin:$VANAHEIMR_INSTALL_PATH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/cuda/bin
 export LD_LIBRARY_PATH=/usr/local/cuda/lib:$LIBCUXX_INSTALL_PATH/lib
 
